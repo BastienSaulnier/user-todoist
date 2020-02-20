0e580306604684801140c4813f55417d4381219a
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-20 10:12:35 +0100

    created assign task button component

---
+++ src/components/AssignButton/AssignButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AssignButton.scss";
+
+export default function AssignButton() {
+  return (
+    <button className="assignButton">
+      <img src="/img/assign_button.png" alt="" /> assigner à
+    </button>
+  );
+}
