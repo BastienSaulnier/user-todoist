425e4c9c9aed47f6a0bc1f6c2095ad76523bb01d
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-20 10:11:47 +0100

    created ended tasks display component

---
+++ src/components/EndedTasksTitle/EndedTasksTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./EndedTasksTitle.scss";
+
+export default function EndedTasksTitle() {
+  return (
+    <div className="endedTasksTitle">
+      <p>Taches terminées</p>
+      <p>1</p>
+    </div>
+  );
+}
