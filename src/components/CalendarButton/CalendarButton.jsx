cc1128eaae80aaa81e6c0d979016b6aec6fa6d36
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-20 10:12:48 +0100

    created calendar tasl component button

---
+++ src/components/CalendarButton/CalendarButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CalendarButton.scss";
+
+export default function CalendarButton() {
+  return (
+    <button className="calendarButton">
+      <img src="/img/calendar_button.png" alt="" />
+      échéance
+    </button>
+  );
+}
