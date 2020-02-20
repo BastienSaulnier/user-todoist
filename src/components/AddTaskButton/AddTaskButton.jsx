b77d3db12b4ecbe837eceeb7abd505ec881b297a
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-20 10:10:22 +0100

    created add tasks component

---
+++ src/components/AddTaskButton/AddTaskButton.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./AddTaskButton.scss";
+
+export default function AddTaskButon() {
+  return <button className="addTaskButton">+ Ajouter une tache...</button>;
+}
