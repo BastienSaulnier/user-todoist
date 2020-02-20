3da6b7d65f5de728b4103fec0d3eebdcb32b815f
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-20 10:14:07 +0100

    created commment form button component

---
+++ src/components/CommentButton/CommentButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CommentButton.scss";
+
+export default function CommentButton() {
+  return (
+    <div className="commentButtonContainer">
+      <button className="commentButton">Commenter</button>
+    </div>
+  );
+}
