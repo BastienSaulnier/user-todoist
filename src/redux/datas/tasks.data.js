11ad4d473b15deb481a3ca353d539bbe20e16f04
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-27 15:13:28 +0100

    simuled datas

---
+++ src/redux/datas/tasks.data.js
@@ -0,0 +1,52 @@
+export const tasks = [
+  {
+    id: 1,
+    taskName: "Task 1",
+    taskUser: "user5",
+    taskUserPic: "",
+    taskDate: "",
+    taskComment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget bibendum tellus. Ut porttitor sem justo, tempor pharetra neque sollicitudin ut. Nullam posuere pretium libero sit amet finibus. Curabitur magna ipsum, pellentesque sit amet porta sit amet, dapibus a erat. ",
+    isResolved: false
+  },
+  {
+    id: 2,
+    taskName: "Task 2",
+    taskUser: "user3",
+    taskUserPic: "",
+    taskDate: "",
+    taskComment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae urna urna. Vivamus nec metus ex. Etiam dolor tortor, auctor nec condimentum eu, iaculis id erat. Donec. ",
+    isResolved: false
+  },
+  {
+    id: 3,
+    taskName: "Task 3",
+    taskUser: "user2",
+    taskUserPic: "",
+    taskDate: "",
+    taskComment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque justo leo, gravida vitae auctor eget, venenatis at mi. Sed hendrerit purus at velit bibendum, ac interdum nibh tincidunt. Cras ultrices at urna ac mattis. Aenean. ",
+    isResolved: true
+  },
+  {
+    id: 4,
+    taskName: "Task 4",
+    taskUser: "user4",
+    taskUserPic: "",
+    taskDate: "",
+    taskComment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget sem eu orci malesuada efficitur. Integer auctor, metus. ",
+    isResolved: false
+  },
+  {
+    id: 5,
+    taskName: "Task 5",
+    taskUser: "user1",
+    taskUserPic: "",
+    taskDate: "",
+    taskComment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est lorem, ornare ut aliquet ac, vehicula ac erat. Morbi sit amet eros non leo consectetur faucibus. Etiam posuere pellentesque commodo. Etiam pulvinar, lacus nec faucibus dapibus, nisl urna suscipit sapien, sed feugiat. ",
+    isResolved: true
+  }
+];
---
+++ src/redux/datas/users.data.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    id: 1,
+    profilePic: "https://randomuser.me/api/portraits/men/87.jpg",
+    userName: "Dylan Lawrence",
+    userEmail: "dylan.lawrence@example.com"
+  },
+  {
+    id: 2,
+    profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
+    userName: "Annette Hernandez",
+    userEmail: "annette.hernandez@example.com"
+  },
+  {
+    id: 3,
+    profilePic: "https://randomuser.me/api/portraits/women/31.jpg",
+    userName: "Andrea Russell",
+    userEmail: "andrea.russell@example.com"
+  },
+  {
+    id: 4,
+    profilePic: "https://randomuser.me/api/portraits/men/89.jpg",
+    userName: "Carter Wilson",
+    userEmail: "carter.wilson@example.com"
+  },
+  {
+    id: 5,
+    profilePic: "https://randomuser.me/api/portraits/women/15.jpg",
+    userName: "Leah Bailey",
+    userEmail: "leah.bailey@example.com"
+  }
+];
