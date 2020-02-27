fc35bec7e723f142817ca81158609562ae3c7e2d
USER 3 <user3@MacBook-Air-de-USER.local>
2020-02-27 15:15:45 +0100

    combined app.js and index.js + made scss reset and scss index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store/index.js";
+import Bar from "./components/Bar/index.js";
+import Home from "./pages/Home/index.js";
+import { Router, Route, Switch } from "react-router-dom";
+import { history } from "./redux/store/index.js";
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+function App() {
+  return (
+    <div className="App">
+      <Bar />
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/:taskName" component={Home} />
+          <Route exact path="/add" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
